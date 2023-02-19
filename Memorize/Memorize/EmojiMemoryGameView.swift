b6cc17fc7affab98d855b99b89ba65568db46996
Jaime Molina <jasmo2.com@gmail.com>
2023-02-19 08:53:51 -0500

    Change file name

---
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,66 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Jasmo Two on 16/02/23.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+  @ObservedObject var viewModel: EmojiMemoryGame
+  
+  var body: some View {
+    return HStack(){
+      ForEach(viewModel.cards) { card in
+        CardView(card: card).onTapGesture {
+          perform: do {
+            viewModel.choose(card: card)
+          }
+        }
+      }
+      
+    }
+    .padding()
+    .foregroundColor(Color.orange)
+    .font(Font.largeTitle)
+    
+  }
+}
+
+struct CardView: View {
+  var card: MemoryGame<String>.Card
+  
+  
+  var body: some View {
+    return ZStack() {
+      if card.isFacedUP {
+        RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+        RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+        Text(card.content)
+      } else {
+        RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+      }
+    }
+  }
+}
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello there, world!")
+//        }
+//        .padding()
+//    }
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    let game = EmojiMemoryGame()
+    EmojiMemoryGameView(viewModel: game)
+  }
+}
