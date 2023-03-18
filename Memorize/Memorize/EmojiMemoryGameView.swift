680f7bbf1667cc925c81764580e41148367422fd
Jaime Molina <jasmo2.com@gmail.com>
2023-03-18 15:08:23 -0500

    Btn and implicit animation,

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -11,17 +11,20 @@ struct EmojiMemoryGameView: View {
   @ObservedObject var viewModel: EmojiMemoryGame
   
   var body: some View {
-    
-    GridStruct(items: viewModel.cards) { card in
-      CardView(card: card).onTapGesture {
-      perform: do {
-        viewModel.choose(card: card)
+    VStack{
+      GridStruct(items: viewModel.cards) { card in
+        CardView(card: card).onTapGesture {
+        perform: do {
+          viewModel.choose(card: card)
+        }
+        }.padding(5)
       }
-      }.padding(5)
+      .padding()
+      .foregroundColor(Color.orange)
+      Button(action: {
+        viewModel.resetGame()
+      }, label: {Text("New Game")})
     }
-    .padding()
-    .foregroundColor(Color.orange)
-    
   }
 }
 
@@ -37,18 +40,20 @@ struct CardView: View {
     }
     
   }
-
+  
   @ViewBuilder
   func body(for size: CGSize) -> some View {
-      ZStack {
-        Pie(startAngel: Angle.degrees(0-90), endAngel: Angle.degrees(110-90), clockwise: true)
-          .opacity(0.4).padding(5)
-        Text(card.content)
-          .font(
-            Font.system(size: fontSize(for: size))
-          )
-      }
-      .cardify(isFacedUp: card.isFacedUp)
+    ZStack {
+      Pie(startAngel: Angle.degrees(0-90), endAngel: Angle.degrees(110-90), clockwise: true)
+        .opacity(0.4).padding(5)
+      Text(card.content)
+        .font(
+          Font.system(size: fontSize(for: size))
+        )
+        .rotationEffect(Angle.degrees(card.isMatched ? 180 : 0))
+        .animation(Animation.easeInOut(duration: 0.350).repeatForever(autoreverses: false), value: card.isMatched)
+    }
+    .cardify(isFacedUp: card.isFacedUp)
   }
   
   func fontSize(for size: CGSize) -> CGFloat {
